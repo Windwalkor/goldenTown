2437aa07001f314b32827439cde8db8aa0c41038
Ghost <ghost@ghostdemacbook-pro.local>
2016-12-01 16:01:33 +0800

    初始化操作

---
+++ goldenTown/goldenTown/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  goldenTown
+//
+//  Created by Ghost on 16/12/1.
+//  Copyright © 2016年 Ghost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ goldenTown/goldenTown/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  goldenTown
+//
+//  Created by Ghost on 16/12/1.
+//  Copyright © 2016年 Ghost. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
